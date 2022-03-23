bd85ac68ae94d502619104666dee804351e83ca8
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-03-23 18:32:34 +0200

    react toggle button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import ThemeImgMoon from './images/icon-moon.svg'
+import ThemeImgSun from './images/icon-sun.svg'
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState(false)
+
+  function themeControl() {
+    setTheme(!theme)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='btn btn-theme-control' aria-pressed={theme} onClick={themeControl}>
+        <span className='sr-only'>{"light theme"}</span>
+        <img className={`light-theme-img ${theme ? "hide-theme-img" : ""}`} src={ThemeImgMoon} alt="" />
+        <img className={`dark-theme-img ${theme ? "" : "hide-theme-img"}`} src={ThemeImgSun} alt="" />
+      </button>
     </div>
   );
 }
